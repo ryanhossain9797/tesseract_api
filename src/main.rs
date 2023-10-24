c63c592feb61eae74b6eebde2c40ce870803b026
Raiyan Hossain <raiyan.hossain@chaldal.net>
2023-10-24 20:20:41 +0600

    Handle all errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
 use base64::{engine::general_purpose, Engine as _};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 struct RequestType {
@@ -12,32 +12,51 @@ struct ResponseType {
     result: String,
 }
 
+async fn get_text_from_image(image_base_64: String) -> Result<String, String> {
+    match general_purpose::STANDARD.decode(image_base_64) {
+        Ok(image_bytes) => match leptess::LepTess::new(None, "eng") {
+            Ok(mut lt) => match lt.set_image_from_mem(&image_bytes) {
+                Ok(_) => match lt.get_utf8_text() {
+                    Ok(ocrText) => Ok(ocrText),
+                    Err(utf8Error) => Err(format!("{utf8Error}")),
+                },
+                Err(pixError) => Err(format!("{pixError}")),
+            },
+            Err(tesseractError) => Err(format!("{tesseractError}")),
+        },
+        Err(decodeError) => Err(format!("{decodeError}")),
+    }
+}
+
 async fn process(request: web::Json<RequestType>) -> impl Responder {
-    let image_bytes = general_purpose::STANDARD.decode(request.image_base_64.clone()).unwrap();
+    let text_result = get_text_from_image(request.image_base_64.clone()).await;
 
-    let mut lt = leptess::LepTess::new(None, "eng").unwrap();
-    lt.set_image_from_mem(&image_bytes).unwrap();
-    let text = lt.get_utf8_text().unwrap();
+    match text_result {
+        Ok(text) => {
+            let response_type = ResponseType {
+                result: format!("{text}"),
+            };
 
-    let response_type = ResponseType {
-        result: format!("{text}"),
-    };
-
-    HttpResponse::Ok().json(response_type)
+            HttpResponse::Ok().json(response_type)
+        }
+        Err(errMsg) => HttpResponse::InternalServerError().json(ResponseType {
+            result: format!("{errMsg}"),
+        }),
+    }
 }
 
 async fn health() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| 
+    HttpServer::new(|| {
         App::new()
             .route("/health", web::get().to(health))
-            .route("/process", web::post().to(process)))
-        .bind("0.0.0.0:9876")?
-        .run()
-        .await
+            .route("/process", web::post().to(process))
+    })
+    .bind("0.0.0.0:9876")?
+    .run()
+    .await
 }
